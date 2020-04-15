e38076cded9a257c7f406a246f06890cc19a4e35
sumanraj <sumanrajbca@gmail.com>
2020-04-15 09:22:58 +0530

    second commit

--- api/super_admin.php
+++ api/super_admin.php
@@ -3,3 +3,5 @@ include_once("config.php");
 $myfunction = new Myfunction();
 $enc_string =  $myfunction->myencryption("select * from login");
 echo $enc_string;
+$dec_string = $myfunction->mydencryption($enc_string);
+echo $dec_string;
